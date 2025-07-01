03f30fc29d4ca12d1a8ce1f249f8bef503bd62ed
Mxolisi Khoza <mxobitcoin@gmail.com>
2025-07-02 01:16:52 +0200

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,18 @@
+-- INNER JOIN to retrieve all bookings and the respective users 
+
+SELECT * FROM Bookings 
+INNER JOIN Users 
+ON Bookings.user_id = Users.user_id; 
+
+-- LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews 
+
+SELECT * FROM Properties 
+RIGHT JOIN Reviews 
+ON Properties.property_id = Reviews.property_id; 
+
+-- FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user 
+
+SELECT * FROM Users 
+FULL OUTER JOIN Bookings 
+ON Users.user_id = Bookings.user_id 
 
