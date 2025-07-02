b56549ac75d498e73005c650aff6dfb9e1cc7f56
Mxolisi Khoza <mxobitcoin@gmail.com>
2025-07-02 17:28:56 +0200

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,7 +1,15 @@
-SELECT b.* 
-FROM Bookings b 
-WHERE user_id IN (
-  SELECT SUM(*) 
-  FROM User u 
-  WHERE b.user_id = u.user_id 
-);
+-- Part 1: Bookings per user
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    COUNT(b.booking_id) AS total_bookings
+FROM 
+    User u
+LEFT JOIN 
+    Booking b ON u.user_id = b.user_id
+GROUP BY 
+    u.user_id, u.first_name, u.last_name, u.email
+ORDER BY 
+    total_bookings DESC;
