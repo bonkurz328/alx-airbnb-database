fa74831b215507a105d5fc1840975e39006f80c8
Mxolisi Khoza <mxobitcoin@gmail.com>
2025-07-02 16:21:37 +0200

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,5 +1,11 @@
-SELECT * FROM Review 
-WHERE rating > 4.0; 
+SELECT * FROM Property 
+WHERE property_id IN (
+    -- Subquery: Get property_ids with AVG(rating) > 4.0
+    SELECT property_id 
+    FROM Review 
+    GROUP BY property_id 
+    HAVING AVG(rating) > 4.0  -- HAVING (not WHERE) for aggregates!
+); 
 
 SELECT * FROM Booking 
 WHERE user_id > 3.0; 
